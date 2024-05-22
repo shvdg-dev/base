762dd7152bd04e6afce969d394aabc137876baa5
StanHvdG <stanhvdg@gmail.com>
2024-05-22 22:46:26 +0200

    Moved contents of partial.js to raw script in partial node

--- internal/document/document.go
+++ internal/document/document.go
@@ -47,5 +47,22 @@ func (d *Document) CreateDocument(request *http.Request, info *info.Info, conten
 // It is used when creating the document and for a snippet when swapping content with HTMX.
 // The included script offers the minimally required functionality for one snippet.
 func (d *Document) CreatePartial(content ...Node) Node {
-	return Div(Group(content), Script(Src("/public/scripts/partial.js"), Defer()))
+	return Div(Group(content), Script(Raw(partialScript)))
 }
+
+const partialScript string = `
+// After loading the DOM
+document.addEventListener("DOMContentLoaded", () => refresh(), {once: true});
+
+/**
+ * Refreshes the web page if there is no <main> element.
+ * Duplicating a tab in the browser fetches the most recent HTML returned from the server.
+ * However, with HTMX, these can be snippets (e.g., for swapping).
+ * A snippet by itself (no linked files etc.) is not useful, hence, a refresh is done.
+ * @return {void} Nothing is returned.
+ */
+function refresh(){
+    if (!document.querySelector("main")) {
+        location.reload();
+    }
+}`

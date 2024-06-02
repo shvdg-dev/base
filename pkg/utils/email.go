49cc2a7022bab64158bc3ada7b41bc59612f97fc
StanHvdG <stanhvdg@gmail.com>
2024-06-02 20:09:49 +0200

    removed pkg

--- pkg/utils/email.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "net/mail"
-
-// IsValidEmail checks if the given email string is a valid email address.
-func IsValidEmail(email string) bool {
-	_, err := mail.ParseAddress(email)
-	return email != "" && err == nil
-}

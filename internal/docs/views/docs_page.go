c1e7be5f01615324212091dcb44033f4ec693a43
StanHvdG <stanhvdg@gmail.com>
2024-06-03 22:50:43 +0200

    Updated view spread

--- internal/docs/views/docs_page.go
+++ internal/docs/views/docs_page.go
@@ -2,20 +2,9 @@ package views
 
 import (
 	consts "base/internal/constants"
-	ctx "base/internal/context"
 	. "github.com/maragudk/gomponents"
 	. "github.com/maragudk/gomponents/html"
 )
-
-// Docs is used for views regarding documentation.
-type Docs struct {
-	Context *ctx.Context
-}
-
-// NewDocs creates a new Docs instance with the given context.
-func NewDocs(context *ctx.Context) *Docs {
-	return &Docs{Context: context}
-}
 
 // CreateDocsPage creates a documentation page.
 func (d *Docs) CreateDocsPage() Node {
--- internal/home/views/home_page.go
+++ internal/home/views/home_page.go
@@ -2,20 +2,9 @@ package views
 
 import (
 	consts "base/internal/constants"
-	ctx "base/internal/context"
 	. "github.com/maragudk/gomponents"
 	. "github.com/maragudk/gomponents/html"
 )
-
-// Home is used for views regarding the home page.
-type Home struct {
-	Context *ctx.Context
-}
-
-// NewHome initializes a new instance of Home.
-func NewHome(context *ctx.Context) *Home {
-	return &Home{Context: context}
-}
 
 // CreateHomePage creates a home page.
 func (h *Home) CreateHomePage() Node {
